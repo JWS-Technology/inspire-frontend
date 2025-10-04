ee755e8935f8fa5ded598d4d99855d8143985424
Joerakesh <rakeshjoe52@gmail.com>
2025-10-04 10:54:59 +0530

    added about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <AboutSection />
     </div>
   );
 }
