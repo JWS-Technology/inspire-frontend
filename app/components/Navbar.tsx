1c2fe37a0ee573d60c34fda1bc9398edbf8e4f80
Joerakesh <rakeshjoe52@gmail.com>
2025-10-04 22:06:01 +0530

    fixed the error which noel caused

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ import NavbarMobile from "./NavbarMobile";
 const Navbar = () => {
   return (
     // fixed across the top, center the nav element (md screens)
-    <div className="pointer-events-none"> 
+    <div className="pointer-events-auto">
       {/* pointer-events-none on outer container prevents accidental blocking; inner nav will have pointer-events-auto */}
       <div className="hidden md:flex fixed inset-x-0 top-3 justify-center z-[100] pointer-events-auto">
         <NavbarDesktop />
