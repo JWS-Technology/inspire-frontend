cccfbd6968ffdf17b145bb5a9797914e0f4b3df5
Joerakesh <rakeshjoe52@gmail.com>
2025-10-05 18:53:55 +0530

    added footer to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar";
 import AboutSection from "../components/AboutSection";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <div>
             <Navbar />
             <AboutSection />
-
+            <Footer />
         </div>
     );
 }
