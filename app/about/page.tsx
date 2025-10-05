0b629d65655fdfa5d13beb38abf5c561996f7d94
Joerakesh <rakeshjoe52@gmail.com>
2025-10-05 20:41:35 +0530

    separated sections

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,18 @@
 import Navbar from "../components/Navbar";
-import AboutSection from "../components/AboutSection";
+import Hero from "../components/about/Hero";
+import CoreValues from "../components/about/CoreValues";
+import Milestone from "../components/about/Milestone";
+import CTASection from "../components/about/CTASection";
 import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <AboutSection />
+            <Hero />
+            <CoreValues />
+            <Milestone />
+            {/* <CTASection /> */}
             <Footer />
         </div>
     );
