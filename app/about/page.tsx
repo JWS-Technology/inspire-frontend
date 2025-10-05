c5c725065410cb2adf1feb6d0834310635ec440f
Joerakesh <rakeshjoe52@gmail.com>
2025-10-05 18:46:27 +0530

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import AboutSection from "../components/AboutSection";
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar />
+            <AboutSection />
+
+        </div>
+    );
+}
